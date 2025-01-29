68aad536ab8587c3b116683f0ea0362ad2035423
noodle <peacheslupi@gmail.com>
2025-01-28 18:46:40 -0800

    Update Sofa.java

--- src/main/java/FurnitureApp/FurnitureInfo/Sofa.java
+++ src/main/java/FurnitureApp/FurnitureInfo/Sofa.java
@@ -6,6 +6,14 @@ public class Sofa implements Furniture
     private String name;
     private int model;
     private int price;
+    private final String type;
+
+    public Sofa(String name, int model, int price) {
+        this.name = name;
+        this.model = model;
+        this.price = price;
+        this.type = "Sofa";
+    }
 
     @Override
     public void setName(String name)
@@ -42,4 +50,11 @@ public class Sofa implements Furniture
     {
         return price;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    //Need to add .equals and hashcode
+    
 }
